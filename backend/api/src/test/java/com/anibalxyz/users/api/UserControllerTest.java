df51e8bdce5c40f5cb126fcf03fdf67eb33d0519
Anibal Boggio <anibalboggioict@gmail.com>
2025-09-04 21:52:57 -0300

    test(api): add getUserById() unit tests

--- backend/api/src/test/java/com/anibalxyz/users/api/UserControllerTest.java
+++ backend/api/src/test/java/com/anibalxyz/users/api/UserControllerTest.java
@@ -5,7 +5,10 @@ import com.anibalxyz.users.application.UserService;
 import com.anibalxyz.users.domain.Email;
 import com.anibalxyz.users.domain.PasswordHash;
 import com.anibalxyz.users.domain.User;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.validation.Validator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -15,10 +18,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
@@ -30,7 +35,10 @@ public class UserControllerTest {
   @InjectMocks private UserController userController;
 
   @Test
-  public void getAllUsers() {
+  @DisplayName("Given there are users, when getAllUsers is called, then return users as JSON")
+  public void getAllUsers_returnUsersJson() {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    // Given there are users
     List<User> fakeUsers =
         List.of(
             new User(
@@ -38,28 +46,113 @@ public class UserControllerTest {
                 "John Doe",
                 new Email("john.doe@example.com"),
                 PasswordHash.generate("12345678"),
-                LocalDateTime.now(),
-                LocalDateTime.now()),
+                localDateTime,
+                localDateTime),
             new User(
                 2,
                 "Jane Smith",
                 new Email("jane.smith@example.com"),
                 PasswordHash.generate("87654321"),
-                LocalDateTime.now(),
-                LocalDateTime.now()));
+                localDateTime,
+                localDateTime));
 
+    // When getAllUsers is called
     when(this.userService.getAllUsers()).thenReturn(fakeUsers);
-
     this.userController.getAllUsers(this.ctx);
 
     ArgumentCaptor<List<UserDetailResponse>> captor = ArgumentCaptor.forClass(List.class);
     verify(this.ctx).json(captor.capture());
-    List<UserDetailResponse> response = captor.getValue();
+    List<UserDetailResponse> actual = captor.getValue();
+    List<UserDetailResponse> expected =
+        fakeUsers.stream().map(UserDetailResponse::fromDomain).toList();
 
-    assertThat(response).hasSize(2);
-    assertThat(response.get(0).id()).isEqualTo(fakeUsers.get(0).getId());
-    assertThat(response.get(0).name()).isEqualTo(fakeUsers.get(0).getName());
-    assertThat(response.get(0).email()).isEqualTo(fakeUsers.get(0).getEmail().value());
-    assertThat(response.get(1).id()).isEqualTo(fakeUsers.get(1).getId());
+    // then return users as JSON
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("Given there are no users, when getAllUsers is called, then return empty JSON array")
+  public void getAllUsers_returnEmptyJsonArray() {
+    List<User> fakeUsers = List.of();
+
+    when(this.userService.getAllUsers()).thenReturn(fakeUsers);
+    this.userController.getAllUsers(this.ctx);
+
+    ArgumentCaptor<List<UserDetailResponse>> captor = ArgumentCaptor.forClass(List.class);
+    verify(this.ctx).json(captor.capture());
+    List<UserDetailResponse> actual = captor.getValue();
+    List<UserDetailResponse> expected = List.of();
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("Given a user ID, when getUserById is called, then return user as JSON")
+  public void getUserById_userExists_returnUserJson() {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    int id = 1;
+    User fakeUser =
+        new User(
+            id,
+            "John Doe",
+            new Email("johndoe@gmail.com"),
+            PasswordHash.generate("12345678"),
+            localDateTime,
+            localDateTime);
+    Optional<User> optionalUser = Optional.of(fakeUser);
+
+    when(this.userService.getUserById(id)).thenReturn(optionalUser);
+
+    Validator<Integer> mockValidator = mock(Validator.class);
+    when(mockValidator.getOrThrow(any())).thenReturn(id);
+    when(this.ctx.pathParamAsClass("id", Integer.class)).thenReturn(mockValidator);
+
+    this.userController.getUserById(this.ctx);
+
+    ArgumentCaptor<UserDetailResponse> captor = ArgumentCaptor.forClass(UserDetailResponse.class);
+    verify(this.ctx).json(captor.capture());
+    UserDetailResponse actual = captor.getValue();
+    UserDetailResponse expected = UserDetailResponse.fromDomain(fakeUser);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("Given a user ID that does not exist, when getUserById is called, then return 404")
+  public void getUserById_userDoesNotExist_return404() {
+    int id = 1;
+
+    Validator<Integer> mockValidator = mock(Validator.class);
+    when(mockValidator.getOrThrow(any())).thenReturn(id);
+    when(this.ctx.pathParamAsClass("id", Integer.class)).thenReturn(mockValidator);
+
+    when(this.userService.getUserById(id)).thenReturn(Optional.empty());
+    when(this.ctx.status(anyInt())).thenReturn(this.ctx);
+
+    this.userController.getUserById(this.ctx);
+
+    verify(this.ctx).status(404);
+    ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
+    verify(this.ctx).json(captor.capture());
+
+    Map<String, String> actual = captor.getValue();
+    Map<String, String> expected = Map.of("error", "User with id " + id + " not found");
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName(
+      "Given an invalid user ID, when getUserById is called, then throw BadRequestResponse")
+  public void getUserById_invalidId_throwsBadRequestResponse() {
+    Validator<Integer> mockValidator = mock(Validator.class);
+    // The message is not really being tested, just that it throws the exception
+    when(mockValidator.getOrThrow(any()))
+        .thenThrow(new BadRequestResponse("Invalid ID format. Must be a number."));
+    when(this.ctx.pathParamAsClass("id", Integer.class)).thenReturn(mockValidator);
+
+    // What really tests is that the exception is thrown and not caught
+    assertThatThrownBy(() -> this.userController.getUserById(this.ctx))
+        .isInstanceOf(BadRequestResponse.class);
   }
 }
