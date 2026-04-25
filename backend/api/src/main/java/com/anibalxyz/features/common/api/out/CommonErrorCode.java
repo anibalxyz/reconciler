b8e79ded2630ffe7cd15b8c55a6516d684e56c04
Anibal Boggio <anibalboggioict@gmail.com>
2026-04-25 19:59:41 -0300

    fix(api): update UNAUTHORIZED error title

--- backend/api/src/main/java/com/anibalxyz/features/common/api/out/CommonErrorCode.java
+++ backend/api/src/main/java/com/anibalxyz/features/common/api/out/CommonErrorCode.java
@@ -3,7 +3,7 @@ package com.anibalxyz.features.common.api.out;
 public enum CommonErrorCode implements ErrorCode {
   VALIDATION_ERROR("The provided data contains validation errors"),
   RESOURCE_NOT_FOUND("The requested resource was not found"),
-  UNAUTHORIZED("Auth access"),
+  UNAUTHORIZED("Access denied"),
   INTERNAL_SERVER_ERROR("An internal server error occurred"),
   UNAVAILABLE_SERVICE("Service temporarily unavailable"),
   BAD_REQUEST("Invalid or malformed request");
