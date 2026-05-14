4a19f5cb6cd17bd670519773b8f4fc9677327a5a
Anibal Boggio <anibalboggioict@gmail.com>
2026-05-13 21:02:01 -0300

    feat(observability): add LogbackMetrics and FileDescriptorMetrics

--- backend/api/src/main/java/com/anibalxyz/server/config/modules/runtime/MetricsConfig.java
+++ backend/api/src/main/java/com/anibalxyz/server/config/modules/runtime/MetricsConfig.java
@@ -6,6 +6,8 @@ import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.prometheusmetrics.PrometheusConfig;
@@ -34,6 +36,8 @@ public class MetricsConfig extends RuntimeConfig {
     new JvmThreadMetrics().bindTo(registry);
     new ProcessorMetrics().bindTo(registry);
     new UptimeMetrics().bindTo(registry);
+    new LogbackMetrics().bindTo(registry);
+    new FileDescriptorMetrics().bindTo(registry);
 
     server.before(
         ctx -> {
